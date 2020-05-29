77cba4ff636f53f1aa2566902ac75e8fb3a22ddc
Rickard Andersson <gonz@severnatazvezda.com>
2020-05-29 18:41:49 +0300

    use sqlite3 from source instead

--- build.zig
+++ build.zig
@@ -4,9 +4,11 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("sqlite-sandbox", "src/main.zig");
     exe.linkLibC();
+    exe.addCSourceFile(
+        "./dependencies/sqlite-amalgamation-3310100/sqlite3.c",
+        &[_][]const u8{"--std=c90"},
+    );
     exe.addIncludeDir("./dependencies/sqlite-amalgamation-3310100");
-    exe.addLibPath("./dependencies/sqlite-amalgamation-3310100");
-    exe.linkSystemLibrary("sqlite3");
     exe.setBuildMode(mode);
     exe.install();
 
